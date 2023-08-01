1fc4bc77ca282795eb3222fa05fcfcace616dfdd
Sri Lakshmi Iyengar K R <110050350+Srilakshmi01112000@users.noreply.github.com>
2023-08-01 11:12:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+function removeArticles(bandName) {
+  const articles = ['the', 'a', 'an'];
+  const nameWords = bandName.split(' ');
+  const filteredWords = nameWords.filter(word => !articles.includes(word.toLowerCase()));
+  return filteredWords.join(' ');
+}
 
+// Function to sort and display band names
+function sortAndDisplayBandNames(bandNames) {
+  // Sorting band names without articles
+  bandNames = bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+  // Add sorted band names to the ul element
+  const bandList = document.getElementById('band');
+  bandList.innerHTML = ''; // Clear any previous items in the list
+  bandNames.forEach(bandName => {
+    const listItem = document.createElement('li');
+    listItem.textContent = bandName;
+    bandList.appendChild(listItem);
+  });
+}
